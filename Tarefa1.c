728d23e7000902bc77f32acf250948a4a3563787
Gabriel Marcone <gabrielmarconems@gmail.com>
2025-01-17 14:21:02 -0300

    adicçao dos pinos de controle do teclado, buzzer e leds.

--- Tarefa1.c
+++ Tarefa1.c
@@ -1,14 +1,28 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
 
+// Definições das linhas e colunas do teclado matricial
+#define ROW1 5
+#define ROW2 6
+#define ROW3 7
+#define ROW4 8
+#define COL1 4
+#define COL2 3
+#define COL3 2
+#define COL4 28
 
+// Definições dos LEDs RGB
+#define LED_RED 13
+#define LED_BLUE 12
+#define LED_GREEN 11
 
-int main()
-{
+// Definição do buzzer
+#define BUZZER 21
+
+int main() {
     stdio_init_all();
+    setup_gpio();
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
     }
+    return 0;
 }
